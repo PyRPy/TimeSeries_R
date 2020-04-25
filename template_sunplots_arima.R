7d0092242e3900a634403e2a9349768edd773bed
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-04-24 23:10:01 -0500

    add 'decompose' and diff12 for exploration...

--- template_sunplots_arima.R
+++ template_sunplots_arima.R
@@ -1,4 +1,6 @@
 library(forecast)
+str(sunspots)
+plot(sunspots)
 fit <- auto.arima(sunspots)
 fit
 forecast(fit, 3)
@@ -6,9 +8,15 @@ accuracy(fit)
 plot(forecast(fit, 6), xlab="Time", ylab="Number")
 
 library(astsa)
-sarima(sunspots, 2, 1, 2)
+sarima(sunspots, 2, 1, 2, 0, 1, 0, 12)
 plot(sunspots)
 acf2(sunspots, 200)
+diff12 <- diff(sunspots, 12)
+plot(diff12)
+
+m <- decompose(sunspots)
+plot(m)
+
 
 diff1 <- diff(sunspots, 1)
 plot(diff1)
@@ -20,4 +28,9 @@ sarima(sunspots, 3, 1, 2)
 # seasonal model
 diff128and1 <- diff(diff1, 128) # crazy
 acf2(diff128and1)
-sarima(sunspots, 0, 1, 1, 1, 0, 0, 128) # crazy
+sarima(sunspots, 0, 1, 1, 1, 0, 0, 12) # crazy
+
+
+require(graphics)
+plot(sunspots, main = "sunspots data", xlab = "Year",
+     ylab = "Monthly sunspot numbers")
