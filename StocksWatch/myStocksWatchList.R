260c9a13fbb45b7ddf9359ccde46fcb0c5ec35fc
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-04-25 22:09:20 -0500

    Check the stocks and trends

---
+++ StocksWatch/myStocksWatchList.R
@@ -0,0 +1,27 @@
+# load packages
+library(quantmod)
+library(PerformanceAnalytics)
+library(xts)
+library(fpp2)
+
+# a collection of stocks
+mystocks <- c("GE", "JNJ", "MSFT", "WMT", "GS", "SBUX")
+
+# read data
+getSymbols(mystocks, src = "yahoo")
+
+# plot the stocks in observation
+autoplot(window(GE$GE.Close, start = "2019-01-01"))
+
+autoplot(window(JNJ$JNJ.Close, start = "2019-01-01"))
+autoplot(window(JNJ$JNJ.Close, start = "2020-01-01"))
+
+autoplot(window(MSFT$MSFT.Close, start = "2019-01-01"))
+
+autoplot(window(WMT$WMT.Close, start = "2019-01-01"))
+autoplot(window(WMT$WMT.Close, start = "2020-01-01"))
+
+autoplot(window(GS$GS.Close, start = "2019-01-01"))
+
+autoplot(window(SBUX$SBUX.Close, start = "2019-01-01"))
+
