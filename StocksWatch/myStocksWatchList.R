8b9667e1e8a13df42d2eebf54b11fe71302c31a8
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-06-07 17:38:48 -0500

    Add stocks ZM and DJI

--- StocksWatch/myStocksWatchList.R
+++ StocksWatch/myStocksWatchList.R
@@ -5,12 +5,15 @@ library(xts)
 library(fpp2)
 
 # a collection of stocks
-mystocks <- c("GE", "JNJ", "MSFT", "WMT", "GS", "SBUX")
+mystocks <- c("GE", "JNJ", "MSFT", "WMT", "GS", "SBUX", "ZM", "^DJI")
 
 # read data
 getSymbols(mystocks, src = "yahoo")
 
 # plot the stocks in observation
+autoplot(window(DJI$DJI.Close, start = "2019-01-01"))
+autoplot(window(DJI$DJI.Close, start = "2020-01-01"))
+
 autoplot(window(GE$GE.Close, start = "2019-01-01"))
 
 autoplot(window(JNJ$JNJ.Close, start = "2019-01-01"))
@@ -24,4 +27,7 @@ autoplot(window(WMT$WMT.Close, start = "2020-01-01"))
 autoplot(window(GS$GS.Close, start = "2019-01-01"))
 
 autoplot(window(SBUX$SBUX.Close, start = "2019-01-01"))
+autoplot(window(SBUX$SBUX.Close, start = "2020-05-01"))
 
+autoplot(window(ZM$ZM.Close, start = "2019-01-01"))
+autoplot(window(ZM$ZM.Close, start = "2020-01-01"))
