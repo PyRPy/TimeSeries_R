241edb6019cc8c79f2bd099348f4e751e846afb9
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-06-08 21:31:18 -0500

    add 200 and 50 day moving average trend - GE/WMT

--- StocksWatch/myStocksWatchList.R
+++ StocksWatch/myStocksWatchList.R
@@ -4,6 +4,9 @@ library(PerformanceAnalytics)
 library(xts)
 library(fpp2)
 
+
+# Stock watch list --------------------------------------------------------
+
 # a collection of stocks
 mystocks <- c("GE", "JNJ", "MSFT", "WMT", "GS", "SBUX", "ZM", "^DJI")
 
@@ -31,3 +34,20 @@ autoplot(window(SBUX$SBUX.Close, start = "2020-05-01"))
 
 autoplot(window(ZM$ZM.Close, start = "2019-01-01"))
 autoplot(window(ZM$ZM.Close, start = "2020-01-01"))
+
+
+# Moving average indicators -----------------------------------------------
+# use WMT as an example 
+sma200 <- SMA(x = Cl(WMT), n = 200)
+sma50 <- SMA(x = Cl(WMT), n = 50)
+plot(window(Cl(WMT), start = "2015-01-01"))
+lines(window(sma200, start = "2015-01-01"), col = "red")
+lines(window(sma50, start = "2015-01-01", col = "blue"))
+
+
+sma200 <- SMA(x = Cl(GE), n = 200)
+sma50 <- SMA(x = Cl(GE), n = 50)
+plot(window(Cl(GE), start = "2015-01-01"))
+lines(window(sma200, start = "2015-01-01"), col = "red")
+lines(window(sma50, start = "2015-01-01", col = "blue"))
+      
