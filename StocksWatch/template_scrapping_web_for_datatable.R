2cb2311438f21728bae8669c6716989a0247f530
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-06-16 18:39:29 -0500

    select 5 stocks for comparison

--- StocksWatch/template_scrapping_web_for_datatable.R
+++ StocksWatch/template_scrapping_web_for_datatable.R
@@ -2,22 +2,26 @@
 
 # install.packages("rvest")
 library(rvest)
+library(quantmod)
 
 link <-paste0("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
 webpage <- read_html(link)
 data <- html_nodes(webpage,".wikitable")
 table<- html_table(data[[1]],header = TRUE)
 # write.csv(table, 'symbols500.csv')
-head(table)
-symbols500 <- table[c("Symbol", "Security")]
 
+head(table)
+symbols500 <- table[c("Symbol", "Security", "GICS Sector")]
+head(symbols500)
 class(symbols500) # still a data.frame in R
 
 numberStocks <- nrow(symbols500)
-idx <- sample(1:numberStocks, 10)
+idx <- sample(1:numberStocks, 5)
 idx
 sampledStocks <- symbols500[idx, "Symbol"]
 sampledStocks
+symbols500[idx, ]
+
 
 stocksEnv <- new.env()
 getSymbols(sampledStocks, env = stocksEnv, from = "2015-01-01", src = "yahoo")
@@ -25,6 +29,8 @@ getSymbols(sampledStocks, env = stocksEnv, from = "2015-01-01", src = "yahoo")
 
 for (stock in sampledStocks) {
   lineChart(stocksEnv[[stock]])
+  # addSMA(n = 50, col = "blue")
+  # addSMA(n = 200, col = "red")
 }
 
 symbols500[idx, ]
